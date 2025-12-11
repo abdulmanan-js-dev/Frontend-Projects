0888977786b66f6b39a740fc99f7a17e3040e274
abdulmanan-js-dev <fokamystic@gmail.com>
2025-12-11 17:08:15 -0500

    Add new elements and styles for task management; enhance accessibility and theming

--- 15. todo-app/src/app.js
+++ 15. todo-app/src/app.js
@@ -1,2 +1,6 @@
 const searchInput = document.querySelector("#search-input");
 const toggleBtn = document.querySelector("#themeToggle");
+const sidebarWrapper = document.querySelector("#sidebar-wrapper");
+const motivationContent = document.querySelector("#motivation-content");
+const totalTasks = document.querySelector("#total-tasks");
+const completedTasks = document.querySelector("#completed-tasks");
